d9d4510ce12f16e634124fd02a67f3a2ff11028b
yapelenua <y.pelenychka@softonix.org>
2026-02-23 18:22:25 +0200

    Creates database indexes
    Improves query performance by creating indexes on frequently queried columns in the Customers, Products, Orders, and OrderItems tables.
    
    Also, sources database creation script to initialize the database and then selects the database for subsequent operations.

--- task.sql
+++ task.sql
@@ -1,13 +1,13 @@
 # Write your SQL code for the database creation here. Good luck! 
 
-CREATE DATABASE IF NOT EXISTS ShopDB;
+SOURCE 'create-database.sql'
 USE ShopDB; 
 
 
-CREATE INDEX Email ON Customers (Email); 
+CREATE INDEX IF NOT EXISTS Email ON Customers (Email); 
 
-CREATE INDEX Name ON Products (Name); 
+CREATE INDEX IF NOT EXISTS Name ON Products (Name); 
 
-CREATE INDEX CustomerID ON Orders (CustomerID); 
-CREATE INDEX OrderID ON OrderItems (OrderID); 
+CREATE INDEX IF NOT EXISTS CustomerID ON Orders (CustomerID); 
+CREATE INDEX IF NOT EXISTS OrderID ON OrderItems (OrderID); 
 CREATE INDEX ProductID ON OrderItems (ProductID);
