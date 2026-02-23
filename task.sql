7078bcfae7b2db5a7dd3c1dea0168da407f2e286
yapelenua <y.pelenychka@softonix.org>
2026-02-23 18:08:29 +0200

    Optimizes database queries with indexes
    Improves query performance by adding indexes to frequently accessed columns in the Customers, Products, and Orders tables.
    
    This enhancement speeds up data retrieval and enhances overall database efficiency.

--- task.sql
+++ task.sql
@@ -2,6 +2,10 @@
 USE ShopDB; 
 
 
-CREATE INDEX idx_email ON Customers (Email); 
+CREATE INDEX Email ON Customers (Email); 
 
-CREATE INDEX idx_name ON Products (Name); 
+CREATE INDEX Name ON Products (Name); 
+
+CREATE INDEX CustomerID ON Orders (CustomerID); 
+CREATE INDEX OrderID ON OrderItems (OrderID); 
+CREATE INDEX ProductID ON OrderItems (ProductID);
